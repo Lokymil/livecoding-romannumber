470ae35d0138b1e0a3f76c4e759d2ee0730059eb
Christophe ARRESTIER <christophe.arrestier.carbon-it@ext.pmu.fr>
2024-10-31 12:12:48 +0100

    Return II & III

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function romanNumber(input) {
+  if (input === 2) {
+    return "II";
+  }
+
+  if (input === 3) {
+    return "III";
+  }
+
   if (input === 5) {
     return "V";
   }
--- index.test.js
+++ index.test.js
@@ -3,6 +3,8 @@ const { romanNumber } = require(".");
 describe("romanNumber", () => {
   it.each([
     { input: 1, output: "I" },
+    { input: 2, output: "II" },
+    { input: 3, output: "III" },
     { input: 5, output: "V" },
   ])("should return $output when given $input", ({ input, output }) => {
     expect(romanNumber(input)).toBe(output);
