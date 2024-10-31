f174a1444378d22d67809348929a9d27a75d9bc9
Christophe ARRESTIER <christophe.arrestier.carbon-it@ext.pmu.fr>
2024-10-31 12:44:38 +0100

    Refacto des XV

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ function romanNumber(input) {
   let roman = "";
   let i = 0;
 
-  if (input >= 5) {
-    roman = "V";
-    i = 5;
+  if (input - i >= 10) {
+    roman += "X";
+    i += 10;
   }
 
-  if (input >= 10) {
-    roman = "X";
-    i = 10;
+  if (input - i >= 5) {
+    roman += "V";
+    i += 5;
   }
 
-  for (; i < input; i++) {
+  for (; i < input; i += 1) {
     roman += "I";
   }
 
--- index.test.js
+++ index.test.js
@@ -9,6 +9,7 @@ describe("romanNumber", () => {
     { input: 6, output: "VI" },
     { input: 7, output: "VII" },
     { input: 10, output: "X" },
+    { input: 15, output: "XV" },
   ])("should return $output when given $input", ({ input, output }) => {
     expect(romanNumber(input)).toBe(output);
   });
